6eabf29391c0b1f74af164b9f03d6d5750d08bae
Yermochenko Sergey <yermochenko@gmail.com>
2025-11-16 22:53:47 +0300

    Пример списка на основе массива
    Добавлена поддержка буферизации

--- 3-generic-and-collections-example/src/main/java/by/vsu/ArrayList.java
+++ 3-generic-and-collections-example/src/main/java/by/vsu/ArrayList.java
@@ -1,35 +1,47 @@
 package by.vsu;
 
 public class ArrayList {
+	private final static int BUFFER_SIZE = 10;
 	private String[] array;
+	private int length;
 
 	public ArrayList() {
-		array = new String[0];
+		array = new String[BUFFER_SIZE];
+		length = 0;
 	}
 
 	public ArrayList(int size) {
 		array = new String[size];
+		length = size;
 	}
 
 	public int size() {
-		return array.length;
+		return length;
 	}
 
 	public String get(int i) {
+		checkIndex(i);
 		return array[i];
 	}
 
 	public void set(int i, String s) {
+		checkIndex(i);
 		array[i] = s;
 	}
 
 	public void add(String s) {
-		String[] newArray = new String[array.length + 1];
-		int i;
-		for(i = 0; i < array.length; i++) {
-			newArray[i] = array[i];
+		if(length == array.length) {
+			String[] newArray = new String[array.length + BUFFER_SIZE];
+			System.arraycopy(array, 0, newArray, 0, array.length);
+			array = newArray;
 		}
-		array = newArray;
-		array[i] = s;
+		array[length] = s;
+		length++;
+	}
+
+	private void checkIndex(int index) {
+		if(index < 0 || index >= length) {
+			throw new ArrayIndexOutOfBoundsException(String.format("Index %d out of bounds for length %d", index, length));
+		}
 	}
 }
